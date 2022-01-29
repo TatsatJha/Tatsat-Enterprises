fd991ac08378d002123a81e00e1f7a2c934ff427
Tatsat Jha <TatsatTarun@gmail.com>
2022-01-29 11:01:46 -0700

    fix styling bug

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -6,10 +6,7 @@ type page= {
 
 function Header(props: page) {
   let color:String = ""
-  useEffect(()=>{
-    color = (props.page === "/dev") ? "blue" : props.page === "/writing" ? "orange" : "teal"
-    console.log(color)
-  })
+  color = (props.page === "/dev") ? "blue" : props.page === "/writing" ? "orange" : "teal"
   return <div> 
         <h1 className= {`text-5xl font-medium text-center border-b-4 border-b-${color}-900 p-8 bg-slate-900 text-${color}-700`}>
           Tatsat Jha

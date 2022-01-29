9f8f737fbb547b78652f564f869ebc2e26977f90
Tatsat Jha <TatsatTarun@gmail.com>
2022-01-29 08:59:48 -0700

    add dynamic styling on header

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
-function Header() {
+type page= {
+  page: string
+}
+
+function Header(props: page) {
+  const color:String = props.page === "/dev" ? "blue" : props.page === "/writing" ? "orange" : "teal"
   return <div> 
-        <h1 className= "text-5xl font-medium text-center border-b-4 border-b-blue-900 p-8 bg-slate-900 text-slate-400">
+        <h1 className= {`text-5xl font-medium text-center border-b-4 border-b-${color}-900 p-8 bg-slate-900 text-${color}-700`}>
           Tatsat Enterprises
         </h1>
     </div>;
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div>
-      <Header />
+      <Header page="/"/>
       <SideBar />
       <Main/>
     </div>

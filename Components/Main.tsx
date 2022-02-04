2843eea44002bd2135881740527cb6aff1d55ce3
Tatsat Jha <TatsatTarun@gmail.com>
2022-02-03 20:41:46 -0700

    Change styling add more content

--- Components/Main.tsx
+++ Components/Main.tsx
@@ -3,21 +3,37 @@ import SideBar from './SideBar';
 import { BsChevronDown } from 'react-icons/bs';
 
 export const MainHome = () => {
-  return <div className="h-screen w-full bg-gray-900 bg-gradient-to-b text-stone-300 pt-20 font-montserrat">
+  return <div className="w-full bg-gray-900 bg-gradient-to-b text-stone-300 pt-20 font-montserrat">
 
-      <h1 className =  "text-center font-light text-5xl border-cyan-700 border-4 w-fit mr-auto ml-auto p-10 text-stone-300 rounded-lg top-0 translate-y-4 duration-75 bg-gray-800 shadow-xl">
-        I <span className='text-orange-700'>Write Fantasy Books</span> and <span className='text-blue-700'>Develop Software</span>
+      <h1 className =  "text-center font-light text-[44px] border-cyan-700  w-fit ml-12 mr-auto p-8 text-stone-300 rounded-lg top-0 translate-y-4 duration-75 shadow-xl">
+        <span className='text-orange-700'>Creative Writer</span> <span className='text-cyan-700'>//</span> <span className='text-blue-700'>Software Developer</span>
       </h1>
     
-      <div className='flex justify-evenly mt-48'>
-        <button className='border-orange-900 border-4 text-orange-900 bg-orange-600 p-5 text-2xl hover:-translate-y-2 duration-300 ease-in-out shadow-xl' > Learn More about My Writing </button>
-        <button className='border-blue-900 border-4 text-blue-900 bg-blue-600 p-5 text-2xl hover:-translate-y-2 duration-300 ease-in-out shadow-xl' > Learn More about My Software </button>
+      <div className='flex justify-evenly mt-64'>
+        <button className='border-orange-900 border-4 text-orange-900 bg-orange-600 p-5 text-2xl hover:-translate-y-2 duration-300 ease-in-out shadow-xl' > 
+          Learn More About My Writing 
+        </button>
+
+        <button className='border-blue-900 border-4 text-blue-900 bg-blue-600 p-5 text-2xl hover:-translate-y-2 duration-300 ease-in-out shadow-xl' > 
+          Learn More About My Software 
+        </button>
       </div>
 
-      <h2 className='text-center font-light text-4xl text-cyan-700 mt-56'>
-        Learn More about Me
-      </h2>
-      <BsChevronDown className='mr-auto ml-auto text-cyan-700 text-5xl'></BsChevronDown>
+      <div className='hover:brightness-150 hover:translate-y-2 duration-300 ease-in-out'>
+        <h2 className='text-center font-light text-4xl text-cyan-700 mt-48 w-fit mr-auto ml-auto'>
+          More About Me
+        </h2>
+        <BsChevronDown className='mr-auto ml-auto text-cyan-700 text-5xl'></BsChevronDown>
+      </div>
+
+      <section className='ml-12'>
+        <h2 className='font-bold text-4xl p-3 m-6'>Who I Am</h2>
+        <p className='font-serif text-xl m-6 w-1/2'> 
+          High school student Based in Phoenix Arizona. I'm passionate about Developing New Projects and Optimizing Processes. I find the best places for Creation to be in Software and Books. I'm working on Developing Progressively more Complex Applications and starting a Business.
+        </p>
+        <h2 className='font-bold text-4xl p-6'> What I Do </h2>
+        <h2 className='font-bold text-4xl p-6'> Experience </h2>
+      </section>
     </div>;
 };
 

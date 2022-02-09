091462b7f13a031cb384634548afc59255d5b376
Tatsat Jha <TatsatTarun@gmail.com>
2022-02-08 20:27:56 -0700

    Finish content and add styling

--- Components/Main.tsx
+++ Components/Main.tsx
@@ -39,17 +39,17 @@ export const MainHome = () => {
       <section className='ml-36 mt-36'>
         <h2 ref= {myRef} className='font-bold text-4xl p-3'> Who I Am </h2>
         <p className='font-serif text-xl m-6 w-1/2'> 
-          High school student Based in Phoenix Arizona. I Enjoy the Process of Creation and Optimization. I find the best places for Creativity to be in Software and Storytelling. As such, I'm working on Developing my skills to create better Software and Write better Books.
+          High school student Based in Phoenix Arizona. I Enjoy the Process of Creation and Optimization, improving processes and scaling operations. I'm passionate about the power of Software and Storytelling and hope to use both to develop products that help the world.
         </p>
         
         <h2 className='font-bold text-4xl p-6'> What I Do </h2>
-        <p className='font-serif text-xl m-6 w-1/2'> 
-          My primary focuses are on the writing of my book series and developing of Software. The foremost Application in my pipeline consists of RainTXT which you can read more about in the dev blog. 
+        <p className='text-right font-serif text-xl m-6 w-1/2'> 
+        I find the best places for Creativity to be in Software and Storytelling. As such, I'm working on Developing my skills to create better Software and Write better Books. The foremost Application in my pipeline consists of RainTXT, an application for authors looking to improve their Storytelling capabilities. As for my Books, I'm working on the first of a planned quintology of books.
         </p>
         
         <h2 className='font-bold text-4xl p-6'> Experience </h2>
-        <p className='font-serif text-xl m-6 w-1/2'> 
-          Working at Press Tecnical Writing as a  Technical Writer
+        <p className='text-left font-serif text-xl m-6 w-1/2'> 
+          Currently working at Press Tecnical Writing as a Technical Writer providing articles on topics such as Binary Trees, Graphs, RestAPIs, Linked Lists and several others to various clientelle 
           Experienced with Full Stack Development technologies such as CSS, HTML, Typescript, ReactJS, NextJS, ElectronJS, and MongoDB as well as Java, Python and Git
           Worked as Technical Director and Mechanical Design Lead at my Robotics Team
           Total of 4 years of experience in Computer Science

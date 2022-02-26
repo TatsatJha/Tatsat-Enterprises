5400f67239b76b797dec9d249e0832149de8139a
Tatsat Jha <TatsatTarun@gmail.com>
2022-02-26 16:13:14 -0700

    add deletion functionality to api and remove clutter

--- pages/api/blog.ts
+++ pages/api/blog.ts
@@ -5,15 +5,34 @@ import blog from "./models/blog"
 
 
 async function handler( req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.method)
-    try {
-      const blogs = await blog.find()
-      res.json(blogs)
-  } catch (err) {
-      res.status(500)
+  switch (req.method){
+    case "GET":
+      try {
+        const blogs = await blog.find()
+        res.json(blogs)
+      } catch (err){res.status(500)}
+      break;
+    case "POST":
+      const blogEntry = new blog({
+        title: req.body.title,
+        content: req.body.content
+      })
+      try {
+        const newBlog = await blogEntry.save()
+        res.status(201).json(newBlog)
+      } catch (error: any) {
+        res.status(400).json(error.message)
+      }
+      break;
+    case "DELETE":
+      try {
+        const blogs = await blog.deleteMany()
+        res.json(blogs)
+      } catch (error:any) {res.status(500)}
+      break;
   }
-  
 }
+  
 export default connectDb(handler)
 // router.get("/", async (req, res) =>{
 //   try {
